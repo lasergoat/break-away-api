06550808124eab205ffafcc53278fe7224405038
Daniel Walker <daniel@fattmerchant.com>
2016-02-27 20:17:32 -0500

    add body parsing and finish api

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var app = express();
 var uuid = require('node-uuid');
+var bodyParser = require('body-parser');
 
-const password = 'THISISTHEPASSWORD';
+app.use(bodyParser.json());
 
-var error = {"error" : "no game in progress"};
+const secret = 'THISISTHEPASSWORD';
 
 var state = {};
 
 var resetState = function() {
   state = {
+    teams: 4,
     lastTeam: 1,
     users: {},
     leaderboard : {
@@ -23,41 +25,54 @@ var resetState = function() {
   };
 };
 
-app.post('/reset/' + password, function(req, res) {
+app.post('/reset/' + secret, function(req, res) {
 
   resetState();
 
-  var types = ['hockey', 'basketball'];
+  var allowedTypes = [
+    'hockey', 'basketball'
+  ];
 
-  if (req.body.game_type && types.indexOf(req.body.game_type) > -1) {
+  if (req.body.game_type && allowedTypes.indexOf(req.body.game_type) > -1) {
     state.game_type = req.body.game_type;
   }
-  res.send('reset', 200);
+
+  if (req.body.teams && typeof req.body.teams === 'number') {
+    state.teams = req.body.teams;
+  }
+
+  res.status(201).json(state);
 });
 
 app.post('/register', function(req, res) {
 
-  if (state === null) {
-    res.send(error, 400);
-    return;
-  }
-
-  res.send({
+  res.status(201).json({
     "game_id": state.game_id,
-    "team": state.lastTeam++ % 4,
+    "team": (state.lastTeam++ % (state.teams))+1,
     "user_id": uuid.v4()
-  }, 201);
-
+  });
 });
 
 app.get('/leaderboard', function(req, res) {
 
-  if (state === null) {
-    res.send(error, 400);
+  res.status(201).json(state.leaderboard);
+});
+
+app.post('/points', function(req, res) {
+
+  if (typeof state.leaderboard[req.body.team] === 'undefined') {
+    res.status(404).json({error: "no team"});
     return;
   }
 
-  res.send(state.leaderboard, 201);
+  if (typeof req.body.points !== "number") {
+    res.status(404).json({error: "invalid points"});
+    return;
+  }
+
+  state.leaderboard[req.body.team] += req.body.points;
+
+  res.status(201).json(1);
 });
 
 resetState();
